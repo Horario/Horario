487a406e2edb3e24d8024aa0ca9514acba96528f
Florian Rietz <52rifl1bwi@hft-stuttgart.de>
2018-04-12 20:54:28 +0200

    - added Listener to TabActivity - ToDo: Other Buttons and Settings functionality

--- app/src/main/java/hft/wiinf/de/horario/TabActivity.java
+++ app/src/main/java/hft/wiinf/de/horario/TabActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import hft.wiinf.de.horario.View.CalendarActivity;
 import hft.wiinf.de.horario.View.NewEventActivity;
@@ -15,6 +16,7 @@ public class TabActivity extends AppCompatActivity {
     private static final String TAG = "TabActivity";
     private SectionsPageAdapterActivity mSectionsPageAdapter;
     private ViewPager mViewPager;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,18 +30,43 @@ public class TabActivity extends AppCompatActivity {
         mViewPager = (ViewPager)findViewById(R.id.container);
         setupViewPager(mViewPager);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabBarLayout);
+       tabLayout = (TabLayout) findViewById(R.id.tabBarLayout);
         tabLayout.setupWithViewPager(mViewPager);
 
         //TODO Change Picture (DesignTeam)
         tabLayout.getTabAt(0).setIcon(R.drawable.ic_android_black_24dp);
         tabLayout.getTabAt(1).setIcon(R.drawable.ic_android_black2_24dp);
         tabLayout.getTabAt(2).setIcon(R.drawable.ic_android_black3_24dp);
+    }
 
+    public void onStart() {
+        super.onStart();
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                if(tab.getPosition() == 2){
+                    mSectionsPageAdapter.getItem(2).getView().findViewById(R.id.settings_relativeLayout_settings).setVisibility(View.GONE);
+                    mSectionsPageAdapter.getItem(2).getView().findViewById(R.id.settings_relativeLayout_main).setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+    }
+
+    public void onResume() {
+        super.onResume();
     }
 
     private void setupViewPager(ViewPager viewPager){
-        SectionsPageAdapterActivity adapter = new SectionsPageAdapterActivity((getSupportFragmentManager()));
+        SectionsPageAdapterActivity adapter = mSectionsPageAdapter;
         adapter.addFragment(new NewEventActivity(),"");
         adapter.addFragment(new CalendarActivity(), "");
         adapter.addFragment(new SettingActivity(), "");
--- app/src/main/java/hft/wiinf/de/horario/View/SettingActivity.java
+++ app/src/main/java/hft/wiinf/de/horario/View/SettingActivity.java
@@ -2,7 +2,9 @@ package hft.wiinf.de.horario.View;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,33 +14,39 @@ import android.widget.RelativeLayout;
 import hft.wiinf.de.horario.R;
 
 
-public class SettingActivity extends Fragment {
+public class SettingActivity extends Fragment{
 
+    //Log.d(TAG, "onTabSelected(): tabId=" + tab.getPosition());
     private static final String TAG = "SettingFragmentActivity";
     Button button_settings, button_support, button_copyright, button_feedback;
     RelativeLayout rLayout_main, rLayout_settings, rLayout_support, rLayout_copyright, rLayout_feedback;
+
+    public SettingActivity (){
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.activity_settings, container, false);
+
         return view;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         //Initialize all Gui-Elements
-        button_settings = (Button)view.findViewById(R.id.settings_button_settings);
-        button_support = (Button)view.findViewById(R.id.settings_button_support);
-        button_copyright = (Button)view.findViewById(R.id.settings_button_copyright);
-        button_feedback = (Button)view.findViewById(R.id.settings_button_feedback);
 
-        rLayout_main = (RelativeLayout)view.findViewById(R.id.settings_relativeLayout_main);
-        rLayout_copyright = (RelativeLayout)view.findViewById(R.id.settings_relativeLayout_copyright);
-        rLayout_feedback = (RelativeLayout)view.findViewById(R.id.settings_relativeLayout_feedback);
-        rLayout_settings = (RelativeLayout)view.findViewById(R.id.settings_relativeLayout_settings);
-        rLayout_support = (RelativeLayout)view.findViewById(R.id.settings_relativeLayout_support);
+        button_settings = (Button) view.findViewById(R.id.settings_button_settings);
+        button_support = (Button) view.findViewById(R.id.settings_button_support);
+        button_copyright = (Button) view.findViewById(R.id.settings_button_copyright);
+        button_feedback = (Button) view.findViewById(R.id.settings_button_feedback);
+
+        rLayout_main = (RelativeLayout) view.findViewById(R.id.settings_relativeLayout_main);
+        rLayout_copyright = (RelativeLayout) view.findViewById(R.id.settings_relativeLayout_copyright);
+        rLayout_feedback = (RelativeLayout) view.findViewById(R.id.settings_relativeLayout_feedback);
+        rLayout_settings = (RelativeLayout) view.findViewById(R.id.settings_relativeLayout_settings);
+        rLayout_support = (RelativeLayout) view.findViewById(R.id.settings_relativeLayout_support);
 
         //Everything that needs to happen after click on "Settings" button
         button_settings.setOnClickListener(new View.OnClickListener() {
@@ -48,12 +56,5 @@ public class SettingActivity extends Fragment {
                 rLayout_settings.setVisibility(View.VISIBLE);
             }
         });
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        rLayout_main.setVisibility(View.VISIBLE);
-        rLayout_settings.setVisibility(View.GONE);
     }
 }
