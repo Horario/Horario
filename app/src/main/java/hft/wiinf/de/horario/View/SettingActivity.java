d4b2c86bb60e58f702af5ddaf79e92a25bd3ab25
Florian Rietz <52rifl1bwi@hft-stuttgart.de>
2018-04-12 17:28:54 +0200

    - created setting view - ToDo: listener to track which fragment is used

--- app/src/main/java/hft/wiinf/de/horario/View/SettingActivity.java
+++ app/src/main/java/hft/wiinf/de/horario/View/SettingActivity.java
@@ -7,34 +7,53 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.RelativeLayout;
 
 import hft.wiinf.de.horario.R;
 
-//TODO Kommentieren und Java Doc Info Schreiben
-public class SettingActivity extends Fragment {
-    private static final String TAG = "SettingFragmentActivity";
-    private Button btnTEST3;
 
+public class SettingActivity extends Fragment {
+
+    private static final String TAG = "SettingFragmentActivity";
+    Button button_settings, button_support, button_copyright, button_feedback;
+    RelativeLayout rLayout_main, rLayout_settings, rLayout_support, rLayout_copyright, rLayout_feedback;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.activity_settings, container, false);
+        return view;
+    }
 
-        View view = inflater.inflate(R.layout.activity_settings, container, false);
-        btnTEST3 = view.findViewById(R.id.btnTEST3);
-        btnTEST3.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        //Initialize all Gui-Elements
+        button_settings = (Button)view.findViewById(R.id.settings_button_settings);
+        button_support = (Button)view.findViewById(R.id.settings_button_support);
+        button_copyright = (Button)view.findViewById(R.id.settings_button_copyright);
+        button_feedback = (Button)view.findViewById(R.id.settings_button_feedback);
+
+        rLayout_main = (RelativeLayout)view.findViewById(R.id.settings_relativeLayout_main);
+        rLayout_copyright = (RelativeLayout)view.findViewById(R.id.settings_relativeLayout_copyright);
+        rLayout_feedback = (RelativeLayout)view.findViewById(R.id.settings_relativeLayout_feedback);
+        rLayout_settings = (RelativeLayout)view.findViewById(R.id.settings_relativeLayout_settings);
+        rLayout_support = (RelativeLayout)view.findViewById(R.id.settings_relativeLayout_support);
+
+        //Everything that needs to happen after click on "Settings" button
+        button_settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(), "Button3 wurde gedrückt", Toast.LENGTH_SHORT).show();
+                rLayout_main.setVisibility(View.GONE);
+                rLayout_settings.setVisibility(View.VISIBLE);
             }
         });
+    }
 
-
-
-
-
-        return view;
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        rLayout_main.setVisibility(View.VISIBLE);
+        rLayout_settings.setVisibility(View.GONE);
     }
 }
