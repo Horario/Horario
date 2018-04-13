b6d35f2fdbf563f5d70476dc149fad733fe9accf
donotreadme <fra.ga@web.de>
2018-04-13 11:40:56 +0200

    merged with master

--- app/src/main/java/hft/wiinf/de/horario/view/CalendarActivity.java
+++ app/src/main/java/hft/wiinf/de/horario/view/CalendarActivity.java
@@ -1,10 +1,14 @@
 package hft.wiinf.de.horario.view;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.github.sundeepk.compactcalendarview.CompactCalendarView;
@@ -15,23 +19,27 @@ import java.util.Date;
 
 import hft.wiinf.de.horario.R;
 
-public class CalendarActivity extends AppCompatActivity {
+//TODO Kommentieren und Java Doc Info Schreiben
+public class CalendarActivity extends Fragment {
+    private static final String TAG = "CalendarFragmentActivity";
 
     public static CompactCalendarView calendarCvCalendar;
     //ListView calendarLvList;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calendar);
 
-        calendarCvCalendar = findViewById(R.id.calendarCvCalendar);
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.activity_calendar, container, false);
+        calendarCvCalendar = view.findViewById(R.id.calendarCvCalendar);
         //calendarLvList = findViewById(R.id.calendarLvList);
 
         calendarCvCalendar.setListener(new CompactCalendarView.CompactCalendarViewListener() {
             @Override
             public void onDayClick(Date dateClicked) {
-                Toast.makeText(CalendarActivity.this, dateClicked.toString(), Toast.LENGTH_SHORT).show(); //TODO just a placeholder, need to be deleted
+                Toast.makeText(getActivity(), dateClicked.toString(), Toast.LENGTH_SHORT).show(); //TODO just a placeholder, need to be deleted
                 //calendarLvList.setAdapter(getAdapter(dateClicked)); //TODO need test
             }
 
@@ -42,6 +50,10 @@ public class CalendarActivity extends AppCompatActivity {
         });
 
 
+
+
+
+        return view;
     }
 
     //TODO just a placeholder, maybe need a rework (1523318400000L)
@@ -53,7 +65,7 @@ public class CalendarActivity extends AppCompatActivity {
     public ArrayAdapter getAdapter(Date date){
         //TODO Datenbank zugriff, um alle Termine für das Datum zu erhalten und diese dann in die List zu speichern.
         ArrayList<String> eventArray = new ArrayList<>();
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, eventArray);
+        ArrayAdapter adapter = new ArrayAdapter(this.getActivity(), android.R.layout.simple_list_item_1, eventArray);
         return adapter;
 
     }
