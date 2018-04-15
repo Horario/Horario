963c1871987c38e15aa81e74d4a418dc583d4302
Frank <52gafr1bwi@hft-stuttgart.de>
2018-04-16 01:15:23 +0200

    Merged with master

--- app/src/main/java/hft/wiinf/de/horario/view/CalendarActivity.java
+++ app/src/main/java/hft/wiinf/de/horario/view/CalendarActivity.java
@@ -1,20 +1,39 @@
 package hft.wiinf.de.horario.view;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 import hft.wiinf.de.horario.R;
 
 //TODO Kommentieren und Java Doc Info Schreiben
 public class CalendarActivity extends Fragment {
     private static final String TAG = "CalendarFragmentActivity";
-    private Button btnTEST2;
+
+    public static CompactCalendarView calendarCvCalendar;
+    ListView calendarLvList;
+    TextView calendarTvMonth;
+    TextView calendarTvDay;
+
+    DateFormat monthFormat = new SimpleDateFormat("MMMM", Locale.GERMAN); //TODO maybe flexible location(like calendar itself)?
 
 
     @Nullable
@@ -23,18 +42,46 @@ public class CalendarActivity extends Fragment {
                              @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.activity_calendar, container, false);
-        btnTEST2 = view.findViewById(R.id.btnTEST2);
-        btnTEST2.setOnClickListener(new View.OnClickListener() {
+        calendarCvCalendar = view.findViewById(R.id.calendarCvCalendar);
+        calendarTvMonth = view.findViewById(R.id.calendarTvMonth);
+        calendarLvList = view.findViewById(R.id.calendarLvList);
+        calendarTvDay = view.findViewById(R.id.calendarTvDay);
+
+        Date today = new Date();
+        calendarTvMonth.setText(monthFormat.format(today)); //initialize month field
+
+        calendarCvCalendar.setListener(new CompactCalendarView.CompactCalendarViewListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(), "Button2 wurde gedrückt", Toast.LENGTH_SHORT).show();
+            public void onDayClick(Date dateClicked) {
+                DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT, Locale.GERMAN);
+                calendarTvDay.setText(df.format(dateClicked));
+                calendarLvList.setAdapter(getAdapter(dateClicked));
             }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                calendarTvMonth.setText(monthFormat.format(firstDayOfNewMonth)); //is updating month field after a swipe
+            }
+
         });
 
 
-
-
-
         return view;
+    }
+
+    //TODO just a placeholder, maybe need a rework (1523318400000L)
+    public static void addEvent(Date date){
+        Event event = new Event(Color.BLUE, date.getTime());
+        calendarCvCalendar.addEvent(event);
+    }
+
+    /** TODO need a description */
+    public ArrayAdapter getAdapter(Date date){
+        //TODO Datenbank zugriff, um alle Termine für das Datum zu erhalten und diese dann in die List zu speichern.
+        ArrayList<String> eventArray = new ArrayList<>();
+        eventArray.add("Test eins"); //TODO just for testing, delete
+        ArrayAdapter adapter = new ArrayAdapter(this.getActivity(), android.R.layout.simple_list_item_1, eventArray);
+        return adapter;
+
     }
 }
