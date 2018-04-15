eb3493d1986803124dc1dca9c44e6a41d150931d
Florian Rietz <52rifl1bwi@hft-stuttgart.de>
2018-04-15 19:44:08 +0200

    - US005: edit manifest to fix bug

--- app/src/main/java/hft/wiinf/de/horario/TabActivity.java
+++ app/src/main/java/hft/wiinf/de/horario/TabActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
+
 import com.activeandroid.ActiveAndroid;
 
 import hft.wiinf.de.horario.view.CalendarActivity;
