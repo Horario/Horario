ec10d546f9a003f2fb4726a7a623878d3eea07c5
Mario Herrmann <61hema1bwi@hft-stuttgart.de>
2018-04-25 11:41:25 +0200

    US008(QRGenerator): - QRGenerator finnish with DummyDates and Old DB Model

--- app/src/main/java/hft/wiinf/de/horario/view/QRGeneratorActivity.java
+++ app/src/main/java/hft/wiinf/de/horario/view/QRGeneratorActivity.java
@@ -1,6 +1,5 @@
 package hft.wiinf.de.horario.view;
 
-import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -12,15 +11,19 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import hft.wiinf.de.horario.R;
+import hft.wiinf.de.horario.controller.EventController;
+import hft.wiinf.de.horario.model.Event;
 
 public class QRGeneratorActivity extends Fragment {
     private static final String TAG = "QRGeneratorFragmentActivity";
@@ -30,16 +33,12 @@ public class QRGeneratorActivity extends Fragment {
     private Button mButtonQRCreation;
     private BitMatrix mBitmatrix;
     private Bitmap mBitmapOfQRCode;
-    private StringBuffer mDataBaseStringBuffer;
-    private String mDataBaseStringResult;
+    private StringBuffer mDataBaseStringBufferResult;
+    private Event mEvent;
+    private Event mEvent2;
 
     public QRGeneratorActivity() {
         // Required empty public constructor
-    }
-
-    @SuppressLint("ResourceType")
-    public void onViewCreated(final View view, Bundle savedInstanceState) {
-
     }
 
     @Override
@@ -48,22 +47,43 @@ public class QRGeneratorActivity extends Fragment {
 
         //Initial GUI
         mButtonQRCreation = view.findViewById(R.id.createQRCode);
-        mQRGenImageViewResult= view.findViewById(R.id.generator_temp_imageView_result);
+        mQRGenImageViewResult= view.findViewById(R.id.generator_imageView_qrResult);
         mRelativeLayout_QRGenResult = view.findViewById(R.id.qr_main);
-        mQRGenTextViewInput = view.findViewById(R.id.generator_temp_imput);
+        mQRGenTextViewInput = view.findViewById(R.id.generator_textView_text_output);
 
-        //Dummydaten für die DB zum Testen des QR Code Generators ... geht noch nicht
-        String place = "Labor";
-        Date startDatum = new Date();
-            String startDate = new SimpleDateFormat("yyyy-MM-dd").format(startDatum);
-        Date endDatum = new Date();
-            String endDate = new SimpleDateFormat("yyyy-MM-dd").format(endDatum);
-        String description = "Experimentelle genetische Versuche mit Überresten von Hr. Albert Einstein. ";
+        //Erstellen von zwei Dummydaten
+        Date startDate = new Date();
+        Date endDate = new Date();
 
-        // Merge the Data Base Informations to One String
-        mDataBaseStringBuffer = new StringBuffer();
-        mDataBaseStringBuffer.append(place+"\n"+description+"\n"+startDate+"\n"+endDate);
-        mDataBaseStringResult = mDataBaseStringBuffer.toString();
+        mEvent = new Event();
+        mEvent.setDescription("Wir machen Experimente im Labor 3");
+        mEvent.setPlace("Labor 3");
+        mEvent.setStartTime(startDate);
+        mEvent.setEndTime(endDate);
+        EventController.addEvent(mEvent);
+
+        mEvent2 = new Event();
+        mEvent.setDescription("Mathe mit Hr. Conradt");
+        mEvent.setPlace("1/208");
+        mEvent.setStartTime(startDate);
+        mEvent.setEndTime(endDate);
+        EventController.addEvent(mEvent2);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(final View view, Bundle savedInstanceState) {
+
+        //Modify the Dateformat form den DB to get a more readable Form
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy 'at' HH:mm:ss");
+
+        // Merge the Data Base Informations to one Single StringBuffer
+        mDataBaseStringBufferResult = new StringBuffer();
+        mDataBaseStringBufferResult.append(mEvent.getDescription()+"\n");
+        mDataBaseStringBufferResult.append(simpleDateFormat.format(mEvent.getStartTime())+"\n");
+        mDataBaseStringBufferResult.append(simpleDateFormat.format(mEvent.getEndTime())+"\n");
+        mDataBaseStringBufferResult.append(mEvent.getPlace()+"\n");
+
 
 
         //Create a QR Code and Show it in the ImageView.
@@ -73,7 +93,8 @@ public class QRGeneratorActivity extends Fragment {
             public void onClick(View v) {
                 MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
                 try {
-                    mBitmatrix = multiFormatWriter.encode(mDataBaseStringResult, BarcodeFormat.QR_CODE,200,200);
+                    //Change the StringBuffer to a String for Output in the LayoutView
+                    mBitmatrix = multiFormatWriter.encode(String.valueOf(mDataBaseStringBufferResult), BarcodeFormat.QR_CODE,200,200);
                     BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
                     mBitmapOfQRCode = barcodeEncoder.createBitmap(mBitmatrix);
                     mQRGenImageViewResult.setImageBitmap(mBitmapOfQRCode);
@@ -83,11 +104,7 @@ public class QRGeneratorActivity extends Fragment {
                 }
             }
         });
-
-        mQRGenTextViewInput.setText(mDataBaseStringResult);
-        return view;
+        mQRGenTextViewInput.setText(mDataBaseStringBufferResult);
     }
-
-
 
 }
