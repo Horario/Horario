e54af6ddf1cf1ff1f95dbc85412e270000a8ec18
MariamBar <52bama1bwi>
2018-04-27 20:30:31 +0200

    Revert "Added buttons and a listener for opening and closing the buttons."
    This reverts commit e3ae2b2eb49a4f25cfe896f74481b9a171f4a3f6.

--- app/src/main/java/hft/wiinf/de/horario/view/NewEventActivity.java
+++ app/src/main/java/hft/wiinf/de/horario/view/NewEventActivity.java
@@ -2,13 +2,10 @@ package hft.wiinf.de.horario.view;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -18,10 +15,6 @@ import hft.wiinf.de.horario.R;
 public class NewEventActivity extends Fragment {
     private static final String TAG = "NewEventFragmentActivity";
     private Button btnTEST1;
-    FloatingActionButton fabOpenClose, fabGoToScanner, fabCreateEvent;
-    Animation ActionButtonOpen, ActionButtonClose, ActionButtonRotateRight, ActionButtonRotateLeft;
-    boolean isActionButtonOpen = false;
-
 
 
     @Nullable
@@ -38,39 +31,6 @@ public class NewEventActivity extends Fragment {
             }
         });
 
-        fabOpenClose = (FloatingActionButton) view.findViewById(R.id.floatingActionButtonOpenClose);
-        fabGoToScanner = (FloatingActionButton) view.findViewById(R.id.floatingActionButtonGoToScanner);
-        fabCreateEvent = (FloatingActionButton) view.findViewById(R.id.floatingActionButtonCreateEvent);
-
-        ActionButtonOpen = AnimationUtils.loadAnimation(getContext(), R.anim.actionbuttonopen);
-        ActionButtonClose = AnimationUtils.loadAnimation(getContext(), R.anim.actionbuttonclose);
-        ActionButtonRotateRight = AnimationUtils.loadAnimation(getContext(), R.anim.actionbuttonrotateright);
-        ActionButtonRotateLeft = AnimationUtils.loadAnimation(getContext(), R.anim.actionbuttonrotateleft);
-
-        fabOpenClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isActionButtonOpen) {
-                    fabGoToScanner.startAnimation(ActionButtonClose);
-                    fabCreateEvent.startAnimation(ActionButtonClose);
-                    fabOpenClose.startAnimation(ActionButtonRotateLeft);
-                    fabGoToScanner.setClickable(false);
-                    fabCreateEvent.setClickable(false);
-                    isActionButtonOpen = false;
-
-                } else {
-                    fabGoToScanner.startAnimation(ActionButtonOpen);
-                    fabCreateEvent.startAnimation(ActionButtonOpen);
-                    fabOpenClose.startAnimation(ActionButtonRotateRight);
-                    fabGoToScanner.setClickable(true);
-                    fabCreateEvent.setClickable(true);
-                    isActionButtonOpen = true;
-
-                }
-
-
-            }
-        });
 
         return view;
     }
