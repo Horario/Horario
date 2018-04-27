e3ae2b2eb49a4f25cfe896f74481b9a171f4a3f6
LucasToulon <lucas.toulon@yahoo.de>
2018-04-27 19:24:01 +0200

    Added buttons and a listener for opening and closing the buttons. NO FUNCTIONALITY on the buttons for QRScanner and CreateEvent
    Comment: Deleted branch, then recreated it because of issues with merging.
    This commit is equal to master status PLUS changes made for US7
    
    4 ERRORS CONTAINED: not my contibution.

--- app/src/main/java/hft/wiinf/de/horario/view/CalendarActivity.java
+++ app/src/main/java/hft/wiinf/de/horario/view/CalendarActivity.java
@@ -3,11 +3,14 @@ package hft.wiinf.de.horario.view;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -36,6 +39,11 @@ public class CalendarActivity extends Fragment {
 
     DateFormat monthFormat = new SimpleDateFormat("MMMM yyyy", Locale.getDefault());
     DateFormat dayFormat = DateFormat.getDateInstance(DateFormat.SHORT, Locale.getDefault());
+
+    FloatingActionButton fabOpenClose, fabGoToScanner, fabCreateEvent;
+    Animation ActionButtonOpen, ActionButtonClose, ActionButtonRotateRight, ActionButtonRotateLeft;
+    boolean isActionButtonOpen = false;
+
 
 
     @Nullable
@@ -77,6 +85,41 @@ public class CalendarActivity extends Fragment {
                 Toast.makeText(getActivity(), "test", Toast.LENGTH_SHORT).show(); //TODO just for testing, delete
             }
         });
+
+        fabOpenClose = (FloatingActionButton) view.findViewById(R.id.floatingActionButtonOpenClose);
+        fabGoToScanner = (FloatingActionButton) view.findViewById(R.id.floatingActionButtonGoToScanner);
+        fabCreateEvent = (FloatingActionButton) view.findViewById(R.id.floatingActionButtonCreateEvent);
+
+        ActionButtonOpen = AnimationUtils.loadAnimation(getContext(), R.anim.actionbuttonopen);
+        ActionButtonClose = AnimationUtils.loadAnimation(getContext(), R.anim.actionbuttonclose);
+        ActionButtonRotateRight = AnimationUtils.loadAnimation(getContext(), R.anim.actionbuttonrotateright);
+        ActionButtonRotateLeft = AnimationUtils.loadAnimation(getContext(), R.anim.actionbuttonrotateleft);
+
+        fabOpenClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isActionButtonOpen) {
+                    fabGoToScanner.startAnimation(ActionButtonClose);
+                    fabCreateEvent.startAnimation(ActionButtonClose);
+                    fabOpenClose.startAnimation(ActionButtonRotateLeft);
+                    fabGoToScanner.setClickable(false);
+                    fabCreateEvent.setClickable(false);
+                    isActionButtonOpen = false;
+
+                } else {
+                    fabGoToScanner.startAnimation(ActionButtonOpen);
+                    fabCreateEvent.startAnimation(ActionButtonOpen);
+                    fabOpenClose.startAnimation(ActionButtonRotateRight);
+                    fabGoToScanner.setClickable(true);
+                    fabCreateEvent.setClickable(true);
+                    isActionButtonOpen = true;
+
+                }
+
+
+            }
+        });
+
 
         return view;
     }
