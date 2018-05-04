f34bde16a400014890efa04ef2986533ea5e5609
Frank <52gafr1bwi@hft-stuttgart.de>
2018-05-04 10:43:49 +0200

    US012: added the option to jump between months in overview

--- app/src/main/java/hft/wiinf/de/horario/view/EventOverviewActivity.java
+++ app/src/main/java/hft/wiinf/de/horario/view/EventOverviewActivity.java
@@ -2,7 +2,9 @@ package hft.wiinf.de.horario.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -18,22 +20,51 @@ public class EventOverviewActivity extends AppCompatActivity {
 
     ListView overviewLvList;
     TextView overviewTvMonth;
+    Button overviewBtNext;
+    Button overviewBtPrevious;
+    Date selectedMonth = new Date(CalendarActivity.selectedMonth.getTime());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //initialize variables
         setContentView(R.layout.activity_event_overview);
         overviewLvList = findViewById(R.id.overviewTvList);
         overviewTvMonth = findViewById(R.id.overviewTvMonth);
-        overviewLvList.setAdapter(iterateOverMonth());
-        overviewTvMonth.setText(CalendarActivity.monthFormat.format(CalendarActivity.selectedMonth));
+        overviewBtNext = findViewById(R.id.overviewBtNext);
+        overviewBtPrevious = findViewById(R.id.overviewBtPrevious);
+
+        update();
+
+
+        overviewBtNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedMonth.setMonth(selectedMonth.getMonth()+1);
+                update();
+            }
+        });
+
+        overviewBtPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedMonth.setMonth(selectedMonth.getMonth()-1);
+                update();
+            }
+        });
     }
 
-    public ArrayAdapter iterateOverMonth(){ //TODO create own Adapter
+    public void update(){
+        overviewTvMonth.setText(CalendarActivity.monthFormat.format(selectedMonth));
+        overviewLvList.setAdapter(iterateOverMonth(selectedMonth));
+    }
+
+    //get all events for the selected month and save them in a adapter
+    public ArrayAdapter iterateOverMonth(Date date){ //TODO create own Adapter
         ArrayList<String> eventArray = new ArrayList<>();
-        Date day = new Date(CalendarActivity.selectedMonth.getTime());
-        int endDate = CalendarActivity.selectedMonth.getMonth();
+        Date day = new Date(date.getTime());
+        int endDate = date.getMonth();
         while (day.getMonth() <= endDate){
             Calendar endOfDay = Calendar.getInstance();
             endOfDay.setTime(day);
@@ -47,6 +78,9 @@ public class EventOverviewActivity extends AppCompatActivity {
             }
             //TODO was wenn keine Termine in diesem Monat sind, irgendeine Message anzeigen? Abhandeln über eventArray size
             day.setTime(endOfDay.getTimeInMillis());
+        }
+        if(eventArray.size() < 1){ //when no events this month do stuff
+            eventArray.add("Du hast keine Termine diesen Monat");
         }
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, eventArray);
         return adapter;
