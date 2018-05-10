e9b4ad1750e3262db72c4c917368656b2de75cad
Mario Herrmann <61hema1bwi@hft-stuttgart.de>
2018-05-10 22:46:45 +0200

    US008_Terminübersicht_als_QR_Code "Pre"finished
    # No Merge with Master .. again!
    # Change the NewEventLink to add some Events!

--- app/src/main/java/hft/wiinf/de/horario/controller/QRScannerController.java
+++
@@ -1,8 +0,0 @@
-package hft.wiinf.de.horario.controller;
-
-public class QRScannerController {
-
-    //ToDo hier muss noch die Logik von QRScannerActivity ausgelagert werden!
-
-
-}
--- app/src/main/java/hft/wiinf/de/horario/view/QRScannerActivity.java
+++ app/src/main/java/hft/wiinf/de/horario/view/QRScannerActivity.java
@@ -270,7 +270,6 @@ public class QRScannerActivity extends Fragment implements ActivityCompat.OnRequ
                     mScannerResult_Button_rejectEvent.setVisibility(View.GONE);
                     mScannerResult_TextureView_Headline.setVisibility(View.GONE);
 
-
                     Snackbar.make(getActivity().findViewById(R.id.scanner_result_relativeLayout_buttonFrame),
                             "Ups! Fehler Aufgetreten!",
                             Snackbar.LENGTH_INDEFINITE).setAction("Zum Kalender", new View.OnClickListener() {
@@ -311,8 +310,6 @@ public class QRScannerActivity extends Fragment implements ActivityCompat.OnRequ
                     }).show();
                 }
             }
-            //mScannerResult_TextureView_Description.setText("Das ist der Inhalt vom QR Code: "+"\n"+qrResult+
-            //        "\n"+"Das können wir leider nicht als Termin speichern!");
             qrResult = null;
         }
     }
