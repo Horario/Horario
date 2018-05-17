7958882b6eed5d26aa02580260fc4f7347b30ad0
DennisRoe <52rode1bwi@hft-stuttgart.de>
2018-05-17 15:56:09 +0200

    on Button (accept, save, reject) click write values in databases Problem: no Repetition in database

--- app/src/main/java/hft/wiinf/de/horario/view/QRScanResultFragment.java
+++ app/src/main/java/hft/wiinf/de/horario/view/QRScanResultFragment.java
@@ -1,6 +1,8 @@
 package hft.wiinf.de.horario.view;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
@@ -11,11 +13,20 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.activeandroid.util.Log;
 
+import java.nio.channels.NonReadableChannelException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import hft.wiinf.de.horario.R;
+import hft.wiinf.de.horario.controller.EventController;
 import hft.wiinf.de.horario.controller.PersonController;
+import hft.wiinf.de.horario.model.AcceptedState;
 import hft.wiinf.de.horario.model.Event;
 import hft.wiinf.de.horario.model.Person;
 import hft.wiinf.de.horario.model.Repetition;
@@ -23,14 +34,19 @@ import hft.wiinf.de.horario.model.Repetition;
 public class QRScanResultFragment extends Fragment {
     private static final String TAG = "QRScanResultFragment";
     private RelativeLayout mScannerResult_RelativeLayout_Main, mScannerResult_RelativeLayout_ButtonFrame,
-            mScannerResult_RelativeLayout_goTo_CalendarFragment;
+            mScannerResult_RelativeLayout_goTo_CalendarFragment, mScannerResult_RelativeLayout_Calendar;
     private TextView mScannerResult_TextureView_Headline, mScannerResult_TextureView_Description;
     private Button mScannerResult_Button_addEvent, mScannerResult_Button_saveWithoutassent,
             mScannerResult_Button_rejectEvent;
     Person person;
     //Index: 0 = CreatorID; 1 = StartDate; 2 = EndDate; 3 = StartTime; 4 = EndTime;
     //       5 = Repetition; 6 = ShortTitle; 7 = Place; 8 = Descriptoin;  9 = EventCreatorName
-    private String creatorID, startDate, endDate, startTime, endTime, repetition, shortTitle, place, description, eventCreatorName;
+    private String creatorID, startDate, endDate, startTime, endTime, repetition, shortTitle, place, description, eventCreatorName, creatorPhoneNumber;
+    private String hourOfDay, minutesOfDay, year, month, day;
+
+    Calendar myStartTime = Calendar.getInstance();
+    Calendar myEndTime = Calendar.getInstance();
+    Calendar myEndDate = Calendar.getInstance();
 
     public QRScanResultFragment() {
         // Required empty public constructor
@@ -45,11 +61,17 @@ public class QRScanResultFragment extends Fragment {
 
     @SuppressLint("ResourceType")
     public void onViewCreated(final View view, Bundle savedInstanceState) {
-
+        myStartTime.set(Calendar.SECOND, 0);
+        myStartTime.set(Calendar.MILLISECOND, 0);
+        myEndTime.set(Calendar.SECOND, 0);
+        myEndTime.set(Calendar.MILLISECOND, 0);
+        myEndDate.set(Calendar.SECOND, 0);
+        myEndDate.set(Calendar.MILLISECOND, 0);
         //GUI initial
         mScannerResult_RelativeLayout_Main = view.findViewById(R.id.scanner_result_relativeLayout_main);
         mScannerResult_RelativeLayout_ButtonFrame = view.findViewById(R.id.scanner_result_relativeLayout_buttonFrame);
         mScannerResult_RelativeLayout_goTo_CalendarFragment = view.findViewById(R.id.scanner_result_realtiveLayout_CalendarFragment);
+        mScannerResult_RelativeLayout_Calendar = view.findViewById(R.id.calendar_constrainLayout_main);
         mScannerResult_TextureView_Description = view.findViewById(R.id.scanner_result_textview_eventText);
         mScannerResult_TextureView_Headline = view.findViewById(R.id.scanner_result_textView_headline);
         mScannerResult_Button_saveWithoutassent = view.findViewById(R.id.scanner_result_button_save_without_assent);
@@ -72,16 +94,78 @@ public class QRScanResultFragment extends Fragment {
             public void onClick(View v) {
                 person = PersonController.getPersonWhoIam();
                 Event event = new Event(person);
-//                event.getAccepted(AcceptedState.ACCEPTED);
+                event.setAccepted(AcceptedState.ACCEPTED);
                 event.setCreatorEventId(Long.parseLong(creatorID.toString()));
-//              event.setStartDate(startDate);
-//                event.setEndDate(endDate);
-//                event.setStartTime(startTime);
-//                event.setEndTime(endTime);
+//                event.setStartEvent();
+                event.setStartTime(getStartTime().getTime());
+                event.setEndTime(getEndTime().getTime());
                 event.setRepetition(getRepetition());
                 event.setShortTitle(shortTitle);
                 event.setPlace(place);
                 event.setDescription(description);
+
+                if (event.getRepetition() != Repetition.NONE) {
+                    event.setEndDate(getEndDate().getTime());
+                    EventController.saveSerialevent(event);
+                } else {
+                    EventController.saveEvent(event);
+                }
+
+                Toast toast = Toast.makeText(view.getContext(), R.string.acceptEvent, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        mScannerResult_Button_rejectEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                person = PersonController.getPersonWhoIam();
+                Event event = new Event(person);
+                event.setAccepted(AcceptedState.REJECTED);
+                event.setCreatorEventId(Long.parseLong(creatorID.toString()));
+//                event.setStartEvent();
+                event.setStartTime(getStartTime().getTime());
+                event.setEndTime(getEndTime().getTime());
+                event.setRepetition(getRepetition());
+                event.setShortTitle(shortTitle);
+                event.setPlace(place);
+                event.setDescription(description);
+
+                if (event.getRepetition() != Repetition.NONE) {
+                    event.setEndDate(getEndDate().getTime());
+                    EventController.saveSerialevent(event);
+                } else {
+                    EventController.saveEvent(event);
+                }
+
+                Toast toast = Toast.makeText(view.getContext(), R.string.rejectEvent, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        mScannerResult_Button_saveWithoutassent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                person = PersonController.getPersonWhoIam();
+                Event event = new Event(person);
+                event.setAccepted(AcceptedState.WAITING);
+                event.setCreatorEventId(Long.parseLong(creatorID.toString()));
+                event.setStartTime(getStartTime().getTime());
+                event.setEndTime(getEndTime().getTime());
+                event.setRepetition(getRepetition());
+                event.setShortTitle(shortTitle);
+                event.setPlace(place);
+                event.setDescription(description);
+
+                if (event.getRepetition() != Repetition.NONE) {
+                    event.setEndDate(getEndDate().getTime());
+                    EventController.saveSerialevent(event);
+                } else {
+                    EventController.saveEvent(event);
+                }
+
+                Toast toast = Toast.makeText(view.getContext(), R.string.saveEvent, Toast.LENGTH_SHORT);
+                toast.show();
             }
         });
     }
@@ -109,6 +193,8 @@ public class QRScanResultFragment extends Fragment {
             //Put StringBufffer in an Array and split the Values to new String Variables
             //Index: 0 = CreatorID; 1 = StartDate; 2 = EndDate; 3 = StartTime; 4 = EndTime;
             //       5 = Repetition; 6 = ShortTitle; 7 = Place; 8 = Descriptoin;  9 = EventCreatorName
+
+            //split qrCodeExample String at |
             String[] eventStringBufferArray = qrScanResultBundle().split("\\|");
             creatorID = eventStringBufferArray[0].trim();
             startDate = eventStringBufferArray[1].trim();
@@ -120,6 +206,7 @@ public class QRScanResultFragment extends Fragment {
             place = eventStringBufferArray[7].trim();
             description = eventStringBufferArray[8].trim();
             eventCreatorName = eventStringBufferArray[9].trim();
+            //creatorPhoneNumber = eventStringBufferArray[10].trim();
 
             // Change the DataBase Repetition Information in a German String for the Repetition Element
             // like "Daily" into "täglich" and so on
@@ -134,7 +221,7 @@ public class QRScanResultFragment extends Fragment {
                     repetition = "wöchentlich";
                     break;
                 case "DAILY":
-                    repetition = "täglisch";
+                    repetition = "täglich";
                     break;
                 case "NONE":
                     repetition = "";
@@ -204,8 +291,8 @@ public class QRScanResultFragment extends Fragment {
         }
     }
 
-    private Repetition getRepetition(){
-        switch(repetition) {
+    private Repetition getRepetition() {
+        switch (repetition) {
             case "jährlich":
                 return Repetition.YEARLY;
             case "monatlich":
@@ -217,6 +304,42 @@ public class QRScanResultFragment extends Fragment {
             default:
                 return Repetition.NONE;
         }
+    }
+    private Calendar getStartTime(){
+        String[] startDateStringBufferArray = startDate.split("\\.");
+        day = startDateStringBufferArray[0].trim();
+        month = startDateStringBufferArray[1].trim();
+        year = startDateStringBufferArray[2].trim();
+
+        String[] startTimeStringBufferArray = startTime.split(":");
+        hourOfDay = startTimeStringBufferArray[0].trim();
+        minutesOfDay = startTimeStringBufferArray[1].trim();
+
+        myStartTime.set(Calendar.HOUR_OF_DAY, Integer.parseInt(hourOfDay));
+        myStartTime.set(Calendar.MINUTE, Integer.parseInt(minutesOfDay));
+        myStartTime.set(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day));
+
+        return myStartTime;
+    }
+
+    private Calendar getEndTime(){
+        String[] endTimeStringBufferArray = endTime.split(":");
+        hourOfDay = endTimeStringBufferArray[0].trim();
+        minutesOfDay = endTimeStringBufferArray[1].trim();
+
+        myEndTime.set(Calendar.HOUR_OF_DAY, Integer.parseInt(hourOfDay));
+        myEndTime.set(Calendar.MINUTE, Integer.parseInt(minutesOfDay));
+
+        return myEndTime;
+    }
+    private Calendar getEndDate(){
+        String[] endDateStringBufferArray = endDate.split("\\.");
+        day = endDateStringBufferArray[0].trim();
+        month = endDateStringBufferArray[1].trim();
+        year = endDateStringBufferArray[2].trim();
+        myEndTime.set(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day));
+
+        return myEndDate;
     }
 }
 
