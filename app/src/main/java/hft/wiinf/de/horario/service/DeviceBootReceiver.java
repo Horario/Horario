0b317d899a2e24dd62bb98fedf1fb4ab610a514c
Florian Rietz <52rifl1bwi@hft-stuttgart.de>
2018-06-02 16:04:31 +0200

    Merge remote-tracking branch 'remotes/origin/master' into US013_SMSSenden
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/java/hft/wiinf/de/horario/TabActivity.java

--- app/src/main/java/hft/wiinf/de/horario/service/DeviceBootReceiver.java
+++ app/src/main/java/hft/wiinf/de/horario/service/DeviceBootReceiver.java
@@ -1,19 +1,11 @@
 package hft.wiinf.de.horario.service;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
-
-import hft.wiinf.de.horario.controller.EventController;
+import hft.wiinf.de.horario.controller.NotificationController;
 import hft.wiinf.de.horario.controller.PersonController;
-import hft.wiinf.de.horario.model.Event;
 import hft.wiinf.de.horario.model.Person;
 
 public class DeviceBootReceiver extends BroadcastReceiver {
@@ -27,42 +19,9 @@ public class DeviceBootReceiver extends BroadcastReceiver {
                 Person notificationPerson = PersonController.getPersonWhoIam();
                 //Only set Alarm if Person wants to receive them
                 if (notificationPerson.isEnablePush()) {
-                    startNotification(context, notificationPerson);
+                    NotificationController.startAlarmForAllEvents(context);
                 }
             }
         }
-    }
-
-    public void startNotification(Context context, Person notificationPerson) {
-        //Get all events that are in the future to set the alarm
-        List<Event> allEvents = EventController.findMyAcceptedEventsInTheFuture();
-        for (Event event : allEvents) {
-            Intent alarmIntent = new Intent(context, NotificationReceiver.class);
-
-            //Get startTime an convert into a Calender to use it
-            Date date = event.getStartTime();
-            Calendar calendar = GregorianCalendar.getInstance();
-            calendar.setTime(date);
-
-            //Put extra Data which is needed for the Notification
-            alarmIntent.putExtra("Event", event.getShortTitle());
-            alarmIntent.putExtra("Hour", calendar.get(Calendar.HOUR_OF_DAY));
-            if (calendar.get(Calendar.MINUTE) < 10) {
-                alarmIntent.putExtra("Minute", "0" + String.valueOf(calendar.get(Calendar.MINUTE)));
-            } else {
-                alarmIntent.putExtra("Minute", String.valueOf(calendar.get(Calendar.MINUTE)));
-            }
-            alarmIntent.putExtra("ID", event.getId().intValue());
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, event.getId().intValue(), alarmIntent, 0);
-
-            //Set AlarmManager --> NotificationReceiver will be called
-            AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-            manager.set(AlarmManager.RTC_WAKEUP, calcNotificationTime(calendar, notificationPerson), pendingIntent);
-        }
-    }
-
-    public long calcNotificationTime(Calendar cal, Person person) {
-        cal.add(Calendar.MINUTE, ((-1) * person.getNotificationTime()));
-        return cal.getTimeInMillis();
     }
 }
