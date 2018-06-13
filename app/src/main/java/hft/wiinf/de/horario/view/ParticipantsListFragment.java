03aaef749f8366fdcc6b532ff9a246c036deb49a
Melanie Strauss <52stme1bwi@hft-stuttgart.de>
2018-06-13 16:31:56 +0200

    design_Rueckmeldungen und Einstellungen angepasst

--- app/src/main/java/hft/wiinf/de/horario/view/ParticipantsListFragment.java
+++ app/src/main/java/hft/wiinf/de/horario/view/ParticipantsListFragment.java
@@ -146,10 +146,10 @@ public class ParticipantsListFragment extends Fragment {
                 // Setting all values in listview
                 if (participantRow.charAt(0) == 'Y') {
                     participant.setText(participantRow.substring(2));
-                    presenceIndicator.setImageResource(R.drawable.ic_fiber_manual_record_green_24dp);
+                    presenceIndicator.setImageResource(R.drawable.ic_mydate_approved_white);
                 } else {
                     participant.setText(participantRow.substring(2));
-                    presenceIndicator.setImageResource(R.drawable.ic_fiber_manual_record_red_24dp);
+                    presenceIndicator.setImageResource(R.drawable.ic_mydate_rejected_white);
                 }
 
                 return item;
